e541672644adddf609324e6b227607845dd85e2c
Lena-2023 <152642024+Lena-2023@users.noreply.github.com>
2024-01-08 19:52:49 +0300

    Add files via upload

--- counter.v
+++ counter.v
@@ -1,25 +1,28 @@
 module counter (
     input wire reset,
     input wire clk,
-    input wire [7:0]wdata,
+    input wire [7:0] wdata,
     input wire wr,
-    output reg [7:0]data,
+    output reg [7:0] data,
     
-    input [7:0] qa
+    output [7:0] qa
    
 );
 always @ (posedge clk or posedge reset)
-   
-   qa = qa + 1;
+    qa = qa + 1;
     
     if (reset)
+       begin
        data <= 8'h00;
-    else
+       end
+  
     if(wr)
     begin
        data <= wdata;
        $display("written %h",wdata);
     end
    else
+      begin
       data <= data + 8'h01;
+      end
 endmodule
